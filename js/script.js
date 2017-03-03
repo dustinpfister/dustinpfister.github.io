e5aba3f02af4a9b1a39210fa31bb00f064cda6b4
dustinpfister <dustin.pfister@gmail.com>
2017-03-03 18:23:09 -0500

    site update 3-3-17

--- js/script.js
+++ js/script.js
@@ -86,6 +86,7 @@
   });
 
   // Caption
+  /*
   $('.article-entry').each(function(i){
     $(this).find('img').each(function(){
       if ($(this).parent().hasClass('fancybox')) return;
@@ -105,6 +106,7 @@
   if ($.fancybox){
     $('.fancybox').fancybox();
   }
+  */
 
   // Mobile nav
   var $container = $('#container'),
